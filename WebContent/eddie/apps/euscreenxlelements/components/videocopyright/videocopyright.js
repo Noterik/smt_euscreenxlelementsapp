9d3d17ce4ad27f73b57d1871fc10a094288d56e3
David Ammeraal <david.ammeraal@gmail.com>
2014-10-03 10:04:01 +0200

    Added videocopyright and viewer component

---
+++ WebContent/eddie/apps/euscreenxlelements/components/videocopyright/videocopyright.js
@@ -0,0 +1,22 @@
+var Videocopyright = function(options){
+	console.log("Videocopyright()");
+	var element = jQuery("#videocopyright");
+	jQuery(window).on('contextmenu', function(event){
+		if(jQuery(event.target).is('video')){
+			event.preventDefault();
+			event.target.pause();
+			var parent = jQuery(event.target).parent();
+			element.width(jQuery(event.target).width());
+			element.height(jQuery(event.target).height());
+			parent.append(element);
+			element.fadeIn("slow");
+		}
+	});
+	
+	element.find('.dismiss a').click(function(){
+		element.fadeOut("slow").then(function(){
+			jQuery('body').append(element);
+		});
+	});
+};
+Videocopyright.prototype = Object.create(Component.prototype);
