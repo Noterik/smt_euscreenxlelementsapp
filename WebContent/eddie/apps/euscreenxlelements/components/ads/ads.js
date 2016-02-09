a85eab86e2df2d178586efc48f6297784b69cbd3
DavidAmmeraal <david.ammeraal@gmail.com>
2016-02-09 16:31:12 +0100

    Added some things for ads

--- WebContent/eddie/apps/euscreenxlelements/components/ads/ads.js
+++ WebContent/eddie/apps/euscreenxlelements/components/ads/ads.js
@@ -1,7 +1,7 @@
 var Ads = function(options){
 	var element = jQuery("#ads");
 	
-	var number = Math.floor(Math.random() * 2) + 1;
+	var number = Math.floor(Math.random() * 3) + 1;
 		
 	element.find('.ads').append(element.find('#ad_template_' + number).text());
 };
