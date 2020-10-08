ec1c3ae85be728fcb679ce1929614a8c220f71c5
Pieter van Leeuwen <p.vanleeuwen@noterik.nl>
2020-10-08 15:31:20 +0200

    https compatibility

--- WebContent/eddie/apps/euscreenxlelements/components/viewer/viewer.js
+++ WebContent/eddie/apps/euscreenxlelements/components/viewer/viewer.js
@@ -76,7 +76,7 @@ Viewer.prototype.setVideo = function(data) {
 
 	if (user != "" && hash != "") {
 		var html = "<script>new EuropeanaMediaPlayer(document.getElementById(\"viewer\"), {manifest: \""
-				+ manurl + "\"}, {editor: \"http://video-editor.eu/user/"
+				+ manurl + "\"}, {editor: \"https://video-editor.eu/user/"
 				+ user
 				+ "/hash/"
 				+ hash
@@ -86,7 +86,7 @@ Viewer.prototype.setVideo = function(data) {
 
 	} else {
 		var html = "<script>new EuropeanaMediaPlayer(document.getElementById(\"viewer\"), {manifest: \""
-				+ manurl + "\"}, {editor: \"http://euscreen.eu/myeuscreen.html\"});</script>";
+				+ manurl + "\"}, {editor: \"https://euscreen.eu/myeuscreen.html\"});</script>";
 	}
 
 	if (this.element.find('video')[0]) {
